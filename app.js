f18f2a85a7fb4edefe6eb68dc473bb08ac4aa9c5
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-30 15:56:35 +0200

    callFixx

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("server in ascolto");
+const express = require("express");
+const connection = require("./data/db");
+const app = express();
+const port = 3000;
+
+// router
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM videogames";
+  // eseguiamo la query!
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(results);
+  });
+});
+
+// listen
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
