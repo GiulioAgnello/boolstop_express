84d8a523f67408da84726e1d8a98eb573ac16331
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-30 17:03:01 +0200

    cors

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // config
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 // require
 const videogamesRouter = require("./routes/videogamesRouter");
 const handlerError = require("./middleware/hendolerror");
 const errorFound = require("./middleware/errorfound");
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
+const urlHost = process.env.APP_HOST;
+const corsConfig = {
+  origin: "http://localhost",
+};
 
+// cors middelware
+app.use(cors(corsConfig));
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
@@ -21,5 +28,5 @@ app.use(errorFound);
 
 // listen
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening at ${urlHost}${port}`);
 });
