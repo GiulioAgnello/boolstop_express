f9736c7c4f00adf438b16a8a701f40195266e00f
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-30 16:03:14 +0200

    dbenv

--- data/db.js
+++ data/db.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql2");
+
+// keys require
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+
+// connection
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "videogames_store",
+  host: `${host}`,
+  user: `${user}`,
+  password: `${password}`,
+  database: `${database}`,
 });
 connection.connect((err) => {
   if (err) throw err;
