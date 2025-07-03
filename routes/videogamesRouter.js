5c39186df176fcb8acd97a766151c7735089f0e3
dannyarena <arenadanny71@gmail.com>
2025-07-03 20:28:06 +0200

    add route to show videogame by slug

--- routes/videogamesRouter.js
+++ routes/videogamesRouter.js
@@ -10,5 +10,6 @@ router.get(`/platform/:platform`, videogamesControllers.indexPlatform);
 router.get(`/genres/:genres`, videogamesControllers.indexRelated);
 router.get(`/platform/:platform/:id`, videogamesControllers.showPlatform);
 router.get(`/:id`, videogamesControllers.show);
+router.get('/slug/:slug', videogamesControllers.showBySlug);
 
 module.exports = router;
