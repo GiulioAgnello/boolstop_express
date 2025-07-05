5085d0345c165fc7b7962349186c9ff2d659055b
dannyarena <arenadanny71@gmail.com>
2025-07-06 00:32:35 +0200

    add function to send email

--- utils/mailer.js
+++ utils/mailer.js
@@ -7,3 +7,21 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS,
     },
 });
+
+const sendOrderConfirmationEmail = async (toEmail, orderDetails) => {
+    const mailOptions = {
+        from: `"Boolstop Shop" <${process.env.EMAIL_USER}>`,
+        to: toEmail,
+        subject: "Conferma del tuo ordine su BoolStop",
+        html: `<h2>Grazie per il tuo ordine!</h2>
+        <p>Riceverai i tuoi videogiochi al piu presto. Ecco i dettagli:</p>
+        <ul>
+            ${orderDetails.items.map(item => `<li>${item.name} x ${item.amount}</li>`).join("")}
+        </ul>
+        <p><strong>Totale:</strong> € ${orderDetails.total}</p>`
+    };
+
+    await transporter.sendMail(mailOptions);
+
+}
+
