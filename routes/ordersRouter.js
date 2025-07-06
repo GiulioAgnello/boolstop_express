2fdf8504fa79609f23649ab1b4fefd7911b062b9
dannyarena <arenadanny71@gmail.com>
2025-07-06 23:22:06 +0200

    Import sendOrderConfirmationEmail utility in ordersRouter

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ordersController = require("../controllers/ordersController");
 const router = express.Router();
+const { sendOrderConfirmationEmail } = require("../utils/mailer");
 
 router.post("/:orderId/videogames", ordersController.addVideogameToOrder);
 router.post("/addOrder", ordersController.addOrder);
